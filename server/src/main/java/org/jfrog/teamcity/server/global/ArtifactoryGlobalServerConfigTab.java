2401e170ea0062de336a4942b2606aabd298cc13
Shay Yaakov <shayy@jfrog.com>
2013-07-07 10:26:21 +0300

    TCAP-173 - Plugin does not respect TeamCity permission settings

--- server/src/main/java/org/jfrog/teamcity/server/global/ArtifactoryGlobalServerConfigTab.java
+++ server/src/main/java/org/jfrog/teamcity/server/global/ArtifactoryGlobalServerConfigTab.java
@@ -16,6 +16,9 @@
 
 package org.jfrog.teamcity.server.global;
 
+import jetbrains.buildServer.serverSide.auth.AuthUtil;
+import jetbrains.buildServer.serverSide.auth.Permission;
+import jetbrains.buildServer.serverSide.auth.SecurityContext;
 import jetbrains.buildServer.web.openapi.PlaceId;
 import jetbrains.buildServer.web.openapi.PositionConstraint;
 import jetbrains.buildServer.web.openapi.SimpleCustomTab;
@@ -32,12 +35,15 @@ import java.util.Map;
 public class ArtifactoryGlobalServerConfigTab extends SimpleCustomTab {
 
     private ServerConfigPersistenceManager configPersistenceManager;
+    private SecurityContext securityContext;
 
     public ArtifactoryGlobalServerConfigTab(final @NotNull WebControllerManager controllerManager,
-            final @NotNull ArtifactoryServerListener serverListener) {
+                                            final @NotNull ArtifactoryServerListener serverListener,
+                                            final @NotNull SecurityContext securityContext) {
         super(controllerManager, PlaceId.ADMIN_SERVER_CONFIGURATION_TAB, ConstantValues.NAME,
                 "artifactoryGlobalServerConfigTab.jsp",
                 "Artifactory");
+        this.securityContext = securityContext;
         configPersistenceManager = serverListener.getConfigModel();
 
         setPosition(PositionConstraint.after("serverConfigGeneral"));
@@ -51,5 +57,19 @@ public class ArtifactoryGlobalServerConfigTab extends SimpleCustomTab {
     public void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request) {
         super.fillModel(model, request);
         model.put("serverConfigPersistenceManager", configPersistenceManager);
+    }
+
+    @Override
+    public boolean isVisible() {
+        return super.isVisible() && userHasPermission();
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull HttpServletRequest request) {
+        return super.isAvailable(request) && userHasPermission();
+    }
+
+    private boolean userHasPermission() {
+        return AuthUtil.hasGlobalPermission(securityContext.getAuthorityHolder(), Permission.CHANGE_SERVER_SETTINGS);
     }
 }
