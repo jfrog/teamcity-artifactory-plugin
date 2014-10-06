71b131e4ee3750a29e93e2c848299f18d7a4abb4
eyalbe4 <eyalbenmoshe@jfrog.com>
2014-10-06 16:15:54 +0300

    TCAP-211 - Compatibility with Gradle 2.x

--- agent/src/main/java/org/jfrog/teamcity/agent/util/ArtifactoryClientConfigurationBuilder.java
+++ agent/src/main/java/org/jfrog/teamcity/agent/util/ArtifactoryClientConfigurationBuilder.java
@@ -208,7 +208,7 @@ public abstract class ArtifactoryClientConfigurationBuilder {
     private static void addMatrixParamProperties(BuildRunnerContext runnerContext,
                                                  ArtifactoryClientConfiguration clientConf) {
         Properties fileAndSystemProperties =
-                BuildInfoExtractorUtils.mergePropertiesWithSystemAndPropertyFile(new Properties(), null);
+                BuildInfoExtractorUtils.mergePropertiesWithSystemAndPropertyFile(new Properties());
         Properties filteredMatrixParams = BuildInfoExtractorUtils
                 .filterDynamicProperties(fileAndSystemProperties, BuildInfoExtractorUtils.MATRIX_PARAM_PREDICATE);
         Enumeration<Object> propertyKeys = filteredMatrixParams.keys();
