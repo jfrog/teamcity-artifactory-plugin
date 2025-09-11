244ed702cbff485fff3c1ba16234328424c7b078
nitinp19 <nitinp@jfrog.com>
2025-09-11 12:21:40 +0530

    Update build info url to be compatible with the Artifact version 7+ (#166)
    * updating to the new url for the artifact version 7+

--- agent/src/main/java/org/jfrog/teamcity/agent/ArtifactoryAgentLifeCycleAdapter.java
+++ agent/src/main/java/org/jfrog/teamcity/agent/ArtifactoryAgentLifeCycleAdapter.java
@@ -3,10 +3,17 @@ package org.jfrog.teamcity.agent;
 import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
 import jetbrains.buildServer.agent.BuildFinishedStatus;
 import jetbrains.buildServer.agent.BuildRunnerContext;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jfrog.build.extractor.BuildInfoExtractorUtils;
 import org.jfrog.teamcity.common.RunnerParameterKeys;
+import org.jfrog.teamcity.common.ConstantValues;
+import jetbrains.buildServer.agent.BuildProgressLogger;
+import org.jfrog.build.extractor.clientConfiguration.client.artifactory.ArtifactoryManager;
+import org.jfrog.build.extractor.UrlUtils;
+import org.jfrog.build.extractor.clientConfiguration.ArtifactoryManagerBuilder;
+import org.jfrog.build.client.ArtifactoryVersion;
+import org.jfrog.teamcity.agent.util.BuildInfoUtils;
 
 import static org.jfrog.teamcity.common.ConstantValues.BUILD_NAME;
 import static org.jfrog.teamcity.common.ConstantValues.BUILD_URL;
@@ -26,21 +33,58 @@ public class ArtifactoryAgentLifeCycleAdapter extends AgentLifeCycleAdapter {
     }
 
    private static void addBuildInfoUrlParam(BuildRunnerContext runner) {
-        String artifactoryUrl = StringUtils.removeEnd(runner.getRunnerParameters().get(RunnerParameterKeys.URL), "/");
+        String configuredUrl = StringUtils.removeEnd(runner.getRunnerParameters().get(RunnerParameterKeys.URL), "/");
+        String buildName =  runner.getRunnerParameters().get(BUILD_NAME);
+        String buildNumber = runner.getBuild().getBuildNumber();
+        String timeStamp = runner.getRunnerParameters().getOrDefault(ConstantValues.PROP_BUILD_TIMESTAMP, "");
+
+        boolean isRTAtleastV7 = isArtifactoryAtleastV7(runner, configuredUrl);
+        
+        // Log build parameters for debugging
+        runner.getBuild().getBuildLogger().message(String.format(
+            "Artifactory Build Info Parameters - Build Name: '%s', Build Number: '%s', Timestamp: '%s', Is V7 Or Above: %s",
+            buildName, buildNumber, timeStamp, isRTAtleastV7
+        ));
+        
         String buildInfoUrl;
-        if (StringUtils.endsWith(artifactoryUrl, "/artifactory")) {
-            buildInfoUrl = createBuildInfoUrl(StringUtils.removeEnd(artifactoryUrl, "/artifactory"), (String)runner.getRunnerParameters().get(BUILD_NAME), runner.getBuild().getBuildNumber(), (String)runner.getRunnerParameters().get("org.jfrog.artifactory.build.timestamp"));
+        if (isRTAtleastV7) {
+            String platformBaseUrl = StringUtils.removeEnd(configuredUrl, "/artifactory");
+            buildInfoUrl = createPlatformBuildInfoUrl(platformBaseUrl, buildName, buildNumber, timeStamp);
         } else {
-            // Without the platform URL it would work only on Artifactory 6
-            buildInfoUrl = BuildInfoExtractorUtils.createBuildInfoUrl(artifactoryUrl, runner.getRunnerParameters().get(BUILD_NAME),
-                    runner.getBuild().getBuildNumber(), "", "", false, false);
+            String serviceBaseUrl = StringUtils.endsWith(configuredUrl, "/artifactory") ?
+                    configuredUrl : configuredUrl + "/artifactory";
+            buildInfoUrl = createLegacyBuildInfoUrl(serviceBaseUrl, buildName, buildNumber);
         }
 
         runner.getBuild().addSharedSystemProperty(BUILD_URL + "." + runner.getBuild().getBuildId() + "." + runner.getId(), buildInfoUrl);
+        // Log the computed Build Info URL to assist users and avoid confusion with legacy library messages
+        runner.getBuild().getBuildLogger().message("Artifactory Build Info URL: " + buildInfoUrl);
    }
 
-   private static String createBuildInfoUrl(String platformUrl, String buildName, String buildNumber, String timeStamp) {
-        String timestampUrlPart = StringUtils.isBlank(timeStamp) ? "" : "/" + timeStamp;
-        return String.format("%s/%s/%s%s/%s", platformUrl + "/ui/builds", buildName, buildNumber, timestampUrlPart, "published");
+   private static boolean isArtifactoryAtleastV7(BuildRunnerContext runner, String selectedServerUrl) {
+        BuildProgressLogger logger = runner.getBuild().getBuildLogger();
+        try {
+            ArtifactoryManagerBuilder builder = BuildInfoUtils.getArtifactoryManagerBuilder(selectedServerUrl, runner.getRunnerParameters(), logger);
+            try (ArtifactoryManager artifactoryManager = builder.build()) {
+                ArtifactoryVersion serverVersion = artifactoryManager.getVersion();
+                return serverVersion.isAtLeast(new ArtifactoryVersion("7.0.0"));
+            }
+        } catch (Exception e) {
+            // If Artifactory version cannot be determined, default to platform (v7+) URL scheme.
+            return true;
+        }
+   }
+
+   private static String createPlatformBuildInfoUrl(String platformUrl, String buildName, String buildNumber, String timeStamp) {
+        String encodedName = UrlUtils.encodeUrlPathPart(buildName);
+        String encodedNumber = UrlUtils.encodeUrlPathPart(buildNumber);
+        String timestampUrlPart = StringUtils.isBlank(timeStamp) ? "" : "/" + (timeStamp);
+        return String.format("%s/%s/%s%s/%s", platformUrl + "/ui/builds", encodedName, encodedNumber, timestampUrlPart, "published");
+   }
+
+   private static String createLegacyBuildInfoUrl(String serviceBaseUrl, String buildName, String buildNumber) {
+        String encodedName = UrlUtils.encodeUrlPathPart(buildName);
+        String encodedNumber = UrlUtils.encodeUrlPathPart(buildNumber);
+        return String.format("%s/%s/%s/%s", serviceBaseUrl, "/webapp/builds", encodedName, encodedNumber);
    }
 }
